b0eb3ac3cc1044855de466dde4948cb21c5bf194
Julia Grigoreva <julia_panina@mail.ru>
2024-01-14 23:37:50 +0300

    fix

---
+++ src/js/tabs.js
@@ -0,0 +1,19 @@
+// Нашли все заголовки табов по data атрибуту
+const tabHeaders = document.querySelectorAll('[data-tab]');
+// Нашли все контент боксы
+const contentBoxes = document.querySelectorAll('[data-tab-content]');
+
+tabHeaders.forEach(function (item) {
+    item.addEventListener('click', function () {
+
+        // 1. Скрыть все content box
+        contentBoxes.forEach(function (item) {
+            item.classList.add('hidden');
+        });
+
+        // 2. Выбрать нужный content box и показать его
+        const contentBox = document.querySelector('#' + this.dataset.tab);
+        contentBox.classList.remove('hidden');
+
+    })
+})
