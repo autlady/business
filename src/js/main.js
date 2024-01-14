2506d6e4f17f3154536bdb7a64a2af9fb1a34d02
Julia Grigoreva <julia_panina@mail.ru>
2024-01-14 22:21:43 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -41,9 +41,6 @@ const swiperPrograms = new Swiper('.programs-slider', {
     900: {
       slidesPerView: 4,
     },
-    // 1200: {
-    //   slidesPerView: 4,
-    // },
   },
 });
 
@@ -79,8 +76,41 @@ const swiperPersons = new Swiper('.persons-slider', {
     900: {
       slidesPerView: 4,
     },
-    // 1200: {
-    //   slidesPerView: 4,
-    // },
+  },
+});
+
+// News slider
+
+const swiperNews = new Swiper('.news-slider', {
+  // Optional parameters
+  slidesPerView: 4,
+  spaceBetween: 20,
+  // loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.program-slider-next',
+    prevEl: '.program-slider-prev',
+  },
+
+  breakpoints: {
+    350: {
+      slidesPerView: 1,
+    },
+    480: {
+      slidesPerView: 2,
+    },
+    580: {
+      slidesPerView: 3,
+    },
+    900: {
+      slidesPerView: 4,
+    },
   },
 });
