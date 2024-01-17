6967e7a3eba1f7d7dcd2f77de9e56a1cc47f89e4
Julia Grigoreva <julia_panina@mail.ru>
2024-01-17 10:36:32 +0300

    fix

--- src/js/select.js
+++ src/js/select.js
@@ -1,8 +1,10 @@
+// клик по кнопке. Открыть/закрыть select
 document.querySelector('.dropdown__button').addEventListener('click', function () {
     document.querySelector('.dropdown__list').classList.toggle('dropdown__list--visible');
     this.classList.add('dropdown__button--active');
 });
 
+// выбор элемента списка. Запомнить выбранное значение. Закрыть дропдаун
 document.querySelectorAll('.dropdown__list-item').forEach(function(listItem){
     listItem.addEventListener('click', function (){
         document.querySelector('.dropdown__button').innerText = this.innerText;
