91ad1ab948eb8efb8ce524d5517ca07ebb0a0936
Julia Grigoreva <julia_panina@mail.ru>
2024-01-17 11:28:16 +0300

    fix

--- src/js/select.js
+++ src/js/select.js
@@ -1,38 +1,42 @@
-const dropDownBtn = document.querySelector('.dropdown__button');
-const dropDownList = document.querySelector('.dropdown__list');
-const dropDownListItems = dropDownList.querySelectorAll('.dropdown__list-item');
-const dropDownInput = document.querySelector('.dropdown__input-hidden')
+document.querySelectorAll('dropdown').forEach(function (dropDownWrapper) {
+    const dropDownBtn = document.querySelector('.dropdown__button');
+    const dropDownList = document.querySelector('.dropdown__list');
+    const dropDownListItems = dropDownList.querySelectorAll('.dropdown__list-item');
+    const dropDownInput = document.querySelector('.dropdown__input-hidden')
 
-// клик по кнопке. Открыть/закрыть select
-dropDownBtn.addEventListener('click', function () {
-    dropDownList.classList.toggle('dropdown__list--visible');
-    this.classList.add('dropdown__button--active');
+    // клик по кнопке. Открыть/закрыть select
+    dropDownBtn.addEventListener('click', function () {
+        dropDownList.classList.toggle('dropdown__list--visible');
+        this.classList.add('dropdown__button--active');
+    });
+
+    // выбор элемента списка. Запомнить выбранное значение. Закрыть дропдаун
+    dropDownListItems.forEach(function (listItem) {
+        listItem.addEventListener('click', function (e) {
+            e.stopPropagation();
+            dropDownBtn.innerText = this.innerText;
+            dropDownBtn.focus();
+            dropDownInput.value = this.dataset.value;
+            dropDownList.classList.remove('dropdown__list--visible');
+        })
+    })
+
+    // клик снаружи дропдауна. Закрыть дропдаун
+    document.addEventListener('click', function (e) {
+        if (e.target !== dropDownBtn) {
+            dropDownBtn.classList.remove('dropdown__button--active');
+            dropDownList.classList.remove('dropdown__list--visible');
+
+        }
+    })
+
+    // нажатие на Tab или Escape. Закрыть дропдаун
+    document.addEventListener('keydown', function (e) {
+        if (e.key === 'Tab' || e.key === 'Escape') {
+            dropDownBtn.classList.remove('dropdown__button--active');
+            dropDownList.classList.remove('dropdown__list--visible');
+        }
+    })
+
 });
 
-// выбор элемента списка. Запомнить выбранное значение. Закрыть дропдаун
-dropDownListItems.forEach(function(listItem){
-    listItem.addEventListener('click', function (e){
-        e.stopPropagation();
-        dropDownBtn.innerText = this.innerText;
-        dropDownBtn.focus();
-        dropDownInput.value = this.dataset.value;
-        dropDownList.classList.remove('dropdown__list--visible');
-    })
-})
-
-// клик снаружи дропдауна. Закрыть дропдаун
-document.addEventListener('click', function (e){
-    if (e.target !== dropDownBtn) {
-        dropDownBtn.classList.remove('dropdown__button--active');
-        dropDownList.classList.remove('dropdown__list--visible');
-
-    }
-})
-
-// нажатие на Tab или Escape. Закрыть дропдаун
-document.addEventListener('keydown', function (e) {
-    if ( e.key === 'Tab' || e.key === 'Escape') {
-        dropDownBtn.classList.remove('dropdown__button--active');
-        dropDownList.classList.remove('dropdown__list--visible');
-    }
-})
