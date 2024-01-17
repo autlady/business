0c2857c0db7a8b3a3bf989d48438ad219eae7cad
Julia Grigoreva <julia_panina@mail.ru>
2024-01-17 10:28:25 +0300

    fix

--- src/js/select.js
+++ src/js/select.js
@@ -1,3 +1,9 @@
 document.querySelector('.dropdown__button').addEventListener('click', function () {
     document.querySelector('.dropdown__list').classList.toggle('dropdown__list--visible');
+});
+
+document.querySelectorAll('.dropdown__list-item').forEach(function(listItem){
+    listItem.addEventListener('click', function (){
+        document.querySelector('.dropdown__button').innerText = this.innerText;
+    })
 })
