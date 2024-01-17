ef8c5a2e8765b5cd6c1cf37c9dfb33510fbe8575
Julia Grigoreva <julia_panina@mail.ru>
2024-01-17 11:45:09 +0300

    fix

--- src/js/select.js
+++ src/js/select.js
@@ -1,8 +1,18 @@
-document.querySelectorAll('dropdown').forEach(function (dropDownWrapper) {
-    const dropDownBtn = document.querySelector('.dropdown__button');
-    const dropDownList = document.querySelector('.dropdown__list');
+// Полифилл для метода forEach для NodeList
+if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = function (callback, thisArg) {
+        thisArg = thisArg || window;
+        for (var i = 0; i < this.length; i++) {
+            callback.call(thisArg, this[i], i, this);
+        }
+    };
+}
+
+document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
+    const dropDownBtn = dropDownWrapper.querySelector('.dropdown__button');
+    const dropDownList = dropDownWrapper.querySelector('.dropdown__list');
     const dropDownListItems = dropDownList.querySelectorAll('.dropdown__list-item');
-    const dropDownInput = document.querySelector('.dropdown__input-hidden')
+    const dropDownInput = dropDownWrapper.querySelector('.dropdown__input-hidden')
 
     // клик по кнопке. Открыть/закрыть select
     dropDownBtn.addEventListener('click', function () {
