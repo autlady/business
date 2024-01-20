66fb23eb207346ff6719d47f7491802e963ec614
Julia Grigoreva <julia_panina@mail.ru>
2024-01-20 15:11:49 +0300

    fix

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,17 +1,21 @@
 const btn = document.querySelector('[data-modal-button]');
 const modal = document.querySelector('[data-modal]');
 const btnClose = document.querySelector('[data-modal-close]');
+const body = document.querySelector('body');
 
 btn.addEventListener('click', function() {
     modal.classList.remove('hidden');
+    body.classList.add('noscroll');
 });
 
 btnClose.addEventListener('click', function() {
     modal.classList.add('hidden');
+    body.classList.remove('noscroll');
 });
 
 modal.addEventListener('click', function() {
     modal.classList.add('hidden');
+    body.classList.remove('noscroll');
 });
 
 modal.querySelector('.modal-window').addEventListener('click', function(e) {
