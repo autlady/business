aa9c1ab005d0821e5921fd2a1dd5081578f1220b
Julia Grigoreva <julia_panina@mail.ru>
2024-01-24 15:28:16 +0300

    fix

--- src/js/expand.js
+++ src/js/expand.js
@@ -1,28 +1,3 @@
-// const expandBtn = document.querySelector('.cases-card__expand');
-
-// console.log(expandBtn)
-
-// const cardText = document.querySelector('.cases-card__text');
-
-// console.log(cardText)
-
-// const closeTextBtn = document.querySelector('.cases-card__close');
-
-// expandBtn.addEventListener('click', function() {
-//         console.log("click on expand!!")
-//         cardText.classList.add("active");
-//         expandBtn.classList.add("hidden");
-//         closeTextBtn.classList.remove("hidden");
-        
-// })
-
-// closeTextBtn.addEventListener('click', function() {
-//     console.log("click on close!!")
-//     cardText.classList.remove("active");
-//     expandBtn.classList.remove("hidden");
-//     closeTextBtn.classList.add("hidden");
-// })
-
 const casesCards = document.querySelectorAll('.cases-card');
 
 for (card of casesCards) {
@@ -30,13 +5,11 @@ for (card of casesCards) {
     const cardText = card.querySelector('.cases-card__text');
     const closeTextBtn = card.querySelector('.cases-card__close');
     expandBtn.addEventListener('click', function() {
-        console.log("click on close button")
         cardText.classList.add("active");
         expandBtn.classList.add("hidden");
         closeTextBtn.classList.remove("hidden");
     });
     closeTextBtn.addEventListener('click', function() {
-        console.log("click on close!!")
         cardText.classList.remove("active");
         expandBtn.classList.remove("hidden");
         closeTextBtn.classList.add("hidden");
