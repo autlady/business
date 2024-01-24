eb70550115be13658c510d328786447861aa3f3d
Julia Grigoreva <julia_panina@mail.ru>
2024-01-24 14:27:48 +0300

    fix

--- src/js/expand.js
+++ src/js/expand.js
@@ -1,31 +1,50 @@
-const expandBtn = document.querySelector('.cases-card__expand');
+// const expandBtn = document.querySelector('.cases-card__expand');
 
-console.log(expandBtn)
+// console.log(expandBtn)
 
-const cardText = document.querySelector('.cases-card__text');
+// const cardText = document.querySelector('.cases-card__text');
 
-console.log(cardText)
+// console.log(cardText)
 
-const closeTextBtn = document.querySelector('.cases-card__close');
+// const closeTextBtn = document.querySelector('.cases-card__close');
 
-expandBtn.addEventListener('click', function() {
-        console.log("click on expand!!")
+// expandBtn.addEventListener('click', function() {
+//         console.log("click on expand!!")
+//         cardText.classList.add("active");
+//         expandBtn.classList.add("hidden");
+//         closeTextBtn.classList.remove("hidden");
+        
+// })
+
+// closeTextBtn.addEventListener('click', function() {
+//     console.log("click on close!!")
+//     cardText.classList.remove("active");
+//     expandBtn.classList.remove("hidden");
+//     closeTextBtn.classList.add("hidden");
+// })
+
+const casesCards = document.querySelectorAll('.cases-card');
+
+for (card of casesCards) {
+    const expandBtn = card.querySelector('.cases-card__expand');
+    const cardText = card.querySelector('.cases-card__text');
+    const closeTextBtn = card.querySelector('.cases-card__close');
+    expandBtn.addEventListener('click', function() {
+        console.log("click on close button")
         cardText.classList.add("active");
         expandBtn.classList.add("hidden");
         closeTextBtn.classList.remove("hidden");
-        
+    });
+    closeTextBtn.addEventListener('click', function() {
+        console.log("click on close!!")
+        cardText.classList.remove("active");
+        expandBtn.classList.remove("hidden");
+        closeTextBtn.classList.add("hidden");
 })
+}
 
-closeTextBtn.addEventListener('click', function() {
-    console.log("click on close!!")
-    cardText.classList.remove("active");
-    expandBtn.classList.remove("hidden");
-    closeTextBtn.classList.add("hidden");
-})
 
-const expandBtns = document.querySelectorAll('.cases-card__expand');
 
-console.log(expandBtns)
 
 
 
