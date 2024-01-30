ac4b9b3f9135f71cfbbdd3f23f279831decc7244
Julia Grigoreva <julia_panina@mail.ru>
2024-01-30 23:20:57 +0300

    fix

--- src/js/expand.js
+++ src/js/expand.js
@@ -4,16 +4,19 @@ for (card of casesCards) {
     const expandBtn = card.querySelector('.cases-card__expand');
     const cardText = card.querySelector('.cases-card__text');
     const closeTextBtn = card.querySelector('.cases-card__close');
-    expandBtn.addEventListener('click', function() {
-        cardText.classList.add("active");
-        expandBtn.classList.add("hidden");
-        closeTextBtn.classList.remove("hidden");
-    });
-    closeTextBtn.addEventListener('click', function() {
-        cardText.classList.remove("active");
-        expandBtn.classList.remove("hidden");
-        closeTextBtn.classList.add("hidden");
-})
+
+    if (expandBtn) {
+        expandBtn.addEventListener('click', function() {
+            cardText.classList.add("active");
+            expandBtn.classList.add("hidden");
+            closeTextBtn.classList.remove("hidden");
+        });
+        closeTextBtn.addEventListener('click', function() {
+            cardText.classList.remove("active");
+            expandBtn.classList.remove("hidden");
+            closeTextBtn.classList.add("hidden");
+    })
+    }
 }
 
 
