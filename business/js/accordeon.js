541bb44b7385b367a63227b1da11f828d1076a37
Julia Grigoreva <julia_panina@mail.ru>
2024-02-03 14:20:43 +0300

    fix

--- business/js/accordeon.js
+++
@@ -1,9 +0,0 @@
-const headers = document.querySelectorAll("[data-name='accordeon-title']");
-
-headers.forEach(function (item) {
-    item.addEventListener('click', showContent);
-})
-
-function showContent() {
-    this.nextElementSibling.classList.toggle('hidden');
-}
