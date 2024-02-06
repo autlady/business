b128ca83a12242dfb64175ae069fe8d22e6d4016
Julia Grigoreva <julia_panina@mail.ru>
2024-02-06 16:30:15 +0300

    fix

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,25 +1,37 @@
-const btn = document.querySelector('[data-modal-button]');
-const modal = document.querySelector('[data-modal]');
-const btnClose = document.querySelector('[data-modal-close]');
+const btns = document.querySelectorAll('[data-modal-button]');
+const modals = document.querySelectorAll('.modal-window');
+const btnsClose = document.querySelectorAll('[data-modal-close]');
 const body = document.querySelector('body');
-if (modal) {
-    btn.addEventListener('click', function() {
-        console.log('click');
-        modal.classList.remove('hidden');
-        body.classList.add('noscroll');
-    });
+if (btns) {
+    for (let btn of btns){
+        btn.addEventListener('click', function(e) {
+            
+            const thisData = btn.getAttribute('data-modal-button');            
+            const thisModal = document.querySelector(`#${thisData}`);
+            thisModal.classList.remove('hidden');
+            body.classList.add('noscroll');
+        });
 
-    btnClose.addEventListener('click', function() {
-        modal.classList.add('hidden');
-        body.classList.remove('noscroll');
-    });
 
-    modal.addEventListener('click', function() {
-        modal.classList.add('hidden');
-        body.classList.remove('noscroll');
-    });
+    }
+    for (let close of btnsClose) {
+        close.addEventListener('click', function (e) {           
+            this.closest('[data-modal]').classList.add('hidden');
+            body.classList.remove('noscroll');
+        });
 
-    modal.querySelector('.modal-window').addEventListener('click', function(e) {
-        e.stopPropagation();
-    });
+
+    }
+    /*клик по полупрозрачной области модалки */
+    for (let modal of modals) {
+        modal.addEventListener('click', function (e) {
+
+            if (e.target == e.currentTarget){
+                modal.closest('[data-modal]').classList.add('hidden');
+                body.classList.remove('noscroll');
+            }
+        });
+
+
+    }
 }
